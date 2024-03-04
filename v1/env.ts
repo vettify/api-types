c379d4579ad67963678c79d4feac0b0b49d7d5c5
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2024-03-04 21:08:25 +0100

    Add gitSha and apiGitSha to /api/v1/env response

--- v1/env.ts
+++ v1/env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 export const envResponse = z.object({
   env: z.string(),
+  gitSha: z.string(),
+  apiGitSha: z.string(),
 });
 
 export type EnvResponse = z.infer<typeof envResponse>;
