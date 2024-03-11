5b4d27c8ac5e38f95adeda3a674ad3d1471bb7a9
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2024-03-11 12:34:57 +0100

    Add asset info to response for /v1/asset/add

--- v1/assets/add.ts
+++ v1/assets/add.ts
@@ -14,6 +14,15 @@ export type AssetsAddRequest = z.infer<typeof assetsAddRequest>;
 
 export const assetsAddResponse = z.object({
   id: z.number(),
+  filename: z.string(),
+  tags: z.string(),
+  description: z.string(),
+  alt_attribute: z.string(),
+  asset_code: z.string(),
+  mimetype: z.string(),
+  size: z.number(),
+  target_url: z.string(),
+  created_at: z.number(),
 });
 
 export type AssetsAddResponse = z.infer<typeof assetsAddResponse>;
